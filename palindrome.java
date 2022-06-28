bcf370f202685df924bc10e3ed9aca42980426bd
Mounish Vatti <76279858+mounishvatti@users.noreply.github.com>
2022-06-28 11:09:51 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,23 +1,16 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+//check whether a string is a palindrome using StringBuilder.
 public class palindrome {
-  //palindrome  
-    public static void main(String[] args){
-        int n=0;
-        boolean check=false;
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("\nEnter the string: ");
         String str = sc.nextLine();
-        sc.close();
-        n=str.length();
-        for (int i=0; i<n; i++){
-            if(str.charAt(i)==str.charAt((n-1)-i)){
-                check=true;
-            }
+        StringBuilder string = new StringBuilder(str);
+        string.reverse();
+        String rev = string.toString();
+        if (str.equals(rev)){
+            System.out.println("Yes it's a palindrome"); 
         }
-        if (check==true){
-            System.out.println("It's a palindrome"); 
-        }
-        else System.out.println("Not a palindrome");          
+        else System.out.println("No it's not a palindrome");          
     }
 }
-
